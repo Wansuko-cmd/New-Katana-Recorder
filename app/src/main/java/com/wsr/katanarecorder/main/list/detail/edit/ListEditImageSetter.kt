570183f4a9e8490db1104b0f0860caee5cb72ec7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-02-13 00:59:56 +0900

    [add] 撮影機能を実装

--- app/src/main/java/com/wsr/katanarecorder/main/list/detail/edit/ListEditImageSetter.kt
+++ app/src/main/java/com/wsr/katanarecorder/main/list/detail/edit/ListEditImageSetter.kt
@@ -3,11 +3,16 @@ package com.wsr.katanarecorder.main.list.detail.edit
 import android.app.Activity
 import android.app.AlertDialog
 import android.net.Uri
+import android.os.Environment
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.wsr.katanarecorder.BuildConfig
+import java.io.File
+import java.util.*
 
 class ListEditImageSetter(
         private val activity: FragmentActivity,
@@ -18,6 +23,8 @@ class ListEditImageSetter(
     private lateinit var dispatchTakePicture: ActivityResultLauncher<Uri>
 
     private val registry = activity.activityResultRegistry
+
+    private  lateinit var uri: Uri
 
     override fun onCreate(owner: LifecycleOwner) {
 
@@ -31,17 +38,38 @@ class ListEditImageSetter(
                         editViewModel.imageUri.postValue(it)
                     }
                 }
+
+        dispatchTakePicture =
+            registry.register(
+                "take-keys",
+                owner,
+                ActivityResultContracts.TakePicture()
+            ){
+                if(it){
+                    editViewModel.imageUri.postValue(uri)
+                }
+            }
     }
 
     fun selectImage() {
-        val items = arrayOf("画像を選択")
+        val items = arrayOf("画像を選択", "撮影する")
 
         AlertDialog.Builder(activity)
                 .setItems(items){ dialog, which ->
                     when(which){
                         0 -> getContent.launch("image/*")
+                        1 -> takePicture()
                     }
                 }
                 .show()
+    }
+
+    private fun takePicture(){
+        val filename = UUID.randomUUID().toString() + ".jpg"
+        val path = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val file = File(path, filename)
+
+        uri = FileProvider.getUriForFile(activity, BuildConfig.APPLICATION_ID + ".provider", file)
+        dispatchTakePicture.launch(uri)
     }
 }
